8703470429211cb94deae5538836c3f280bd3d97
carvas91 <carvas91@hotmail.com>
2024-10-15 10:53:41 -0400

    commit

---
+++ start_training.py
@@ -0,0 +1,7 @@
+import os
+import sys
+
+from networksecurity.logger.logger import logging
+from networksecurity.exception.exception import NetworkSecurityException
+
+from networksecurity.pipeline.training_pipeline import TrainingPipeline
