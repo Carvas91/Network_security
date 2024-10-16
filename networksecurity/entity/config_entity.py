5a35f8e2b1ed968d7ab41a39635d6a52c5bc5d01
carvas91 <carvas91@hotmail.com>
2024-10-16 11:19:47 -0400

    commit

---
+++ networksecurity/entity/config_entity.py
@@ -0,0 +1,13 @@
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logger.logger import logging
+from datetime import datetime
+import os
+import sys
+from networksecurity.constant import training_pipeline
+
+
+
+
+class DataIngestionConfig:
+    def __init__(self):
+        pass
